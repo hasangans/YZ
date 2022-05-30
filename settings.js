112f0ec2fddccb8b4025323c1b94ee8f4a9fb846
hasangans <60222529+hasangans@users.noreply.github.com>
2022-05-30 12:20:03 +0800

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,19 @@
+{
+"AutoRespon": true,
+"AutoMusic": false,
+"AntiDelete": true,
+"autoread": true,
+"autoplaymusic": true,
+"autocomposing": false,
+"autorecording": true,
+"BotName":"LexxMd",  nama bot <-
+"NomorOwner":"6285654783538",  ganti jadi no owner lu <-
+"NomorOwner2":"+6285654783538",  ganti jadi no owner lu<-
+"NamaOwner":"Hasan",  nama owner<-
+"Dana":"087893056652", 
+"Pulsa":"087893056652", 
+"Ovo":"087893056652", 
+"ZeksApi":"twBHoFu3AnqfE6f2rwAuirfTVgh",
+"Zerkey":"ZeroYT7",
+"HunterApi":"Ikyy69"
+}
